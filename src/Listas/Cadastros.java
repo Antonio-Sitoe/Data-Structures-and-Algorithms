c9fef399470f146b2ff83cd66f41e572fb663722
Antonio Sitoe <antoniositoehl@gmail.com>
2023-09-18 15:32:45 +0200

    listas

---
+++ src/Listas/Cadastros.java
@@ -0,0 +1,11 @@
+package Listas;
+
+public class Cadastros {
+  public static void main(String[] args) {
+    Listas lista = new Listas();
+    System.out.println(lista.getTamanho());
+    lista.adicionar("AC");
+    System.out.println(lista.getTamanho());
+
+  }
+}
