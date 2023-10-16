4a33e0508c24112cc3b6dd9cfc8cb9617f22786f
Antonio Sitoe <antoniositoehl@gmail.com>
2023-10-16 08:26:25 +0200

    add exception classes

---
+++ src/HasTable/HashTableEETException.java
@@ -0,0 +1,7 @@
+
+public class HashTableEETException extends Exception {
+
+	public HashTableEETException(String str) {
+		super(str);
+	}
+}
---
+++ src/HasTable/HashTableENETException.java
@@ -0,0 +1,9 @@
+
+public class HashTableENETException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public HashTableENETException(String str) {
+		super(str);
+	}
+
+}
